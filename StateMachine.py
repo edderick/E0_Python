908a3aa944783a4a2c05cd4df13dd3b79a51bb81
Ben <bdudiakfry@gmail.com>
2014-01-14 18:15:03 +0000

    Tested keystream generation against sample dataset 2

--- StateMachine.py
+++ StateMachine.py
@@ -75,7 +75,7 @@ bottomDelay = Component.Delay(Bits('0b00'),topDelay)
 
 T2 = Component.Bijection(Component.Bijection.T2_values, bottomDelay)
 
-blendSlice = Component.Slice(0,1, topDelay)
+blendSlice = Component.Slice(1,2, topDelay)
 
 bigXOR = Component.ExclusiveOR(LFSR1, LFSR2, LFSR3, LFSR4, blendSlice)
 
@@ -87,13 +87,63 @@ blendXOR.addInput(T2)
 output = Component.Output(bigXOR)
 
 for i in range(240):
+    if(i <=40):
+        topDelay.outputs[-1] = Bits('0b00')
+        bottomDelay.outputs[-1] = Bits('0b00')
+    output.step(i)
+
+Z = output.outputs[112:240] #Last 128 bits generated
+
+temp = Z[0]
+for x in Z[1:]:
+    temp+=x
+
+Z = temp
+#print "Z", Z
+update1 = b(Z,0)+b(Z,4)+b(Z,8)+Z[12*8:(12*8)+1]
+
+l2 = BitArray(Z[(12*8)+1:13*8])
+#l2.reverse()
+update2 = b(Z,1)+b(Z,5)+b(Z,9)+l2
+
+update3 = b(Z,2)+b(Z,6)+b(Z,10)+b(Z,13)+Z[15*8:(15*8)+1]
+
+l4 = BitArray(Z[(15*8)+1:16*8])
+#l4.reverse()
+update4 = b(Z,3)+b(Z,7)+b(Z,11)+b(Z,14)+l4
+
+#print "update1", update1
+#print "update2", update2
+#print "update3", update3
+#print "update4", update4
+
+LFSR1.register = update1
+LFSR2.register = update2
+LFSR3.register = update3
+LFSR4.register = update4
+
+#blendXOR.outputs.append(blendXOR.outputs[-1])
+
+#print "len", len(topDelay.outputs), len(bottomDelay.outputs), len(blendXOR.outputs)
+topDelay.outputs.append(topDelay.outputs[-1])
+bottomDelay.outputs.append(bottomDelay.outputs[-1])
+bottomDelay.outputs.append(bottomDelay.outputs[-1])
+
+#while(len(blendXOR.outputs) <= 240):
+ #   blendXOR.outputs.append(Bits('0b00'))
+
+for i in range(240,341):
     output.step(i)
     
-for i in range(240):
-    print i, "Z", output.outputs[i].bin, "LSFRs", LFSR1.outputs[i].bin, LFSR2.outputs[i].bin, LFSR3.outputs[i].bin, LFSR4.outputs[i].bin, "C[t+1]", blendXOR.outputs[i].bin, "C[t]", topDelay.outputs[i].bin 
+#LFSR2 has a problem
+
+for i in range(240,340):
+    print i, "                                 ", LFSR1.outputs[i].bin, LFSR2.outputs[i].bin, LFSR3.outputs[i].bin, LFSR4.outputs[i].bin,"   ",output.outputs[i].bin, blendXOR.outputs[i].bin, "   ",topDelay.outputs[i].bin, bottomDelay.outputs[i].bin #,"blendslice[t]", blendSlice.outputs[i].bin
+    print
+    #print i, "Z", output.outputs[i].bin, "LSFRs", LFSR1.outputs[i].bin, LFSR2.outputs[i].bin, LFSR3.outputs[i].bin, LFSR4.outputs[i].bin, "C[t+1]", blendXOR.outputs[i].bin, "C[t]", topDelay.outputs[i].bin 
     #print i, "LSFR1", LFSR1.register,"LFSR2", LFSR2.register.bin, "LFSR3", LFSR3.register, "LFSR4", foo
     #print i, foo
-    
+    #print i, output.outputs[i].bin
 
 # <codecell>
 
