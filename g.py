7e2d73b9f664d259d44b9b7e0c1f304bfa4d8ac3
Benjamin Dudiak-Fry <bdf1g10@ecs.soton.ac.uk>
2014-01-23 11:51:00 +0000

    datetime pretty prints now

--- g.py
+++ g.py
@@ -2,7 +2,7 @@ import base64
 import Client
 import datetime
 def send_log(recieving, master, keystream, ciphertext, plaintext):
-     d = datetime.now()
+	   d = datetime.datetime.now()
 	   vals = {
 	   "CLK" : clock.uint,
 	   "BD_ADDR" : ID.hex,
@@ -10,6 +10,6 @@ def send_log(recieving, master, keystream, ciphertext, plaintext):
 	   "keystream" : base64.b64encode(keystream.bytes),
 	   "ciphertext" : base64.b64encode(ciphertext.bytes),
 	   "plaintext" : plaintext.bytes,
-	   "timestamp" : d.strftime("[%b %d %H:%M:%S]") }
+	   "timestamp" : d.strftime("%b %d %H:%M:%S") }
 
 	   Client.postTo('localhost', '8000', 'log', vals, 'POST', 'master' if master else 'slave')
